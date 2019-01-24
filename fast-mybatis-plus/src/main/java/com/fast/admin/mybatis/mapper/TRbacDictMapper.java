880bcec25b1c69b5b8adf78fbb41a2ad48b7b55f
ningzuokun <ningzuokun@hzed.com>
2019-01-24 15:14:31 +0800

    fix: 项目结构调整

--- fast-mybatis-plus/src/main/java/com/fast/admin/mybatis/mapper/TRbacDictMapper.java
+++
@@ -1,7 +0,0 @@
-package com.fast.admin.mybatis.mapper;
-
-import com.fast.admin.mybatis.BasicMapper;
-import com.fast.admin.mybatis.entity.TRbacDict;
-
-public interface TRbacDictMapper extends BasicMapper<TRbacDict> {
-}
